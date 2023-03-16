5b930b419a41e987eda75fbe25b34128105d7165
Ian Pratt <ianjdpratt@gmail.com>
2023-03-16 20:10:22 +0000

    CLI is done!

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use color_eyre::eyre::Result;
+use dialoguer::{Input, MultiSelect};
 
 /// An automatic solver for the New York Times' Spelling Bee game, built in Rust.
 #[derive(Debug, Parser)]
@@ -15,6 +17,97 @@ pub struct Cli {
 }
 
 /// Parse the arguments from the user and get the middle and ext. letters.
-pub fn parse_letters() -> (char, [char; 6]) {
-    todo!()
+pub fn parse_letters() -> Result<(char, [char; 6])> {
+    // Parse CLI arguments.
+    let args = Cli::parse();
+
+    // Get middle character.
+    let middle = args
+        .middle
+        .map(|middle| Ok(middle))
+        .unwrap_or_else(prompt_for_middle)?;
+
+    // Get other characters
+    let others = args
+        .others
+        .map(|others| {
+            Ok(others
+                .try_into()
+                .expect("Clap should only parse 6 letters, no more no less!"))
+        })
+        .unwrap_or_else(|| prompt_for_others(middle))?;
+
+    Ok((middle, others))
+}
+
+/// Prompts for the middle letter if it has not been entered.
+fn prompt_for_middle() -> Result<char> {
+    let chosen: char = Input::new()
+        .with_prompt("Enter the letter in the middle of the hexagon")
+        .interact()?;
+
+    return Ok(chosen);
+}
+
+/// Prompts for the other letters if they have not been entered. Makes sure they are
+/// not the middle letter.
+fn prompt_for_others(middle: char) -> Result<[char; 6]> {
+    // let letters: Vec<char> = ('a'..'z').into_iter().filter(|c| *c != middle).collect();
+
+    // let mut chosen: Vec<usize> = vec![];
+
+    // while chosen.len() != 6 {
+    //     chosen = MultiSelect::new()
+    //         .items(&letters)
+    //         .with_prompt("Select the letters on the outer ring of the hexagon")
+    //         .interact()?;
+
+    //     if chosen.len() < 6 {
+    //         println!("Please select more letters (only 6).")
+    //     } else if chosen.len() > 6 {
+    //         println!("Please select less letters (only 6).")
+    //     }
+    // }
+
+    // Ok(chosen
+    //     .iter()
+    //     .map(|idx| letters[*idx])
+    //     .collect::<Vec<_>>()
+    //     .try_into()
+    //     .expect("There should only be six characters here!"))
+
+    loop {
+        let chosen: String = Input::new()
+            .with_prompt("Enter the letters on the outer ring of the hexagon, separated by a space")
+            .interact()?;
+
+        let chosen = chosen.split_whitespace().collect::<Vec<_>>();
+
+        // Report bad input.
+        if chosen.len() > 6 {
+            println!("Too many letters, please try again.");
+            continue;
+        } else if chosen.len() < 6 {
+            println!("Not enough letters, please try again.");
+            continue;
+        }
+
+        let mut others = vec![];
+
+        for letter in chosen {
+            if letter.len() > 1 {
+                println!("'{}' is not a letter, please try again.", letter);
+                continue;
+            }
+
+            others.push(
+                letter
+                    .chars()
+                    .next()
+                    .expect("There should be at least one character!"),
+            );
+        }
+
+        return Ok(others.try_into().expect("This should convert correctly!"));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 mod cli;
 mod solver;
 
-fn main() {
-    let middle = 'i';
-    let others = ['a', 'm', 'y', 'd', 't', 'e'];
+use color_eyre::eyre::Result;
 
-    println!("{:?}", solver::get_possible_words(middle, others));
+fn main() -> Result<()> {
+    // Initialize color-eyre
+    color_eyre::install()?;
+
+    // Get the letters required.
+    let (middle, others) = cli::parse_letters()?;
+
+    // Get the words
+    let words = solver::get_possible_words(middle, others);
+
+    // If there are no words, print an error message.
+    if words.len() == 0 {
+        eprintln!("No possible words! Try checking your input.")
+    }
+    // Otherwise, print them out!
+    else {
+        println!("Possible words: ");
+
+        for word in words {
+            println!("{}", word);
+        }
+    }
+
+    Ok(())
 }
